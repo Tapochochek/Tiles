36e01d57eaf83bd957cae8e40b18986d30ac08c6
Nikonov Roman <roman.nikonov.2017@gmail.com>
2025-11-10 23:57:02 +0500

    Добавлена примитивная система строительства без привязки к команде, простейший рабочий прототип

--- Assets/Scripts/ClickLogick.cs
+++ Assets/Scripts/ClickLogick.cs
@@ -3,26 +3,33 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickLogick : MonoBehaviour
 {
     private string[] turn = { "blue", "red", "green", "purple" };
     private static int turnIndex = 0;
 
-    [SerializeField] GameObject[] builds;
-    [SerializeField] GameObject uiBuildsButton;
-
     public Material mat;
     private List<Material> listMaterials;
     private List<MeshRenderer> meshRenderers;
 
+    [System.Serializable]
+    public class BuildingEntry
+    {
+        public GameObject building;
+        public Button button;
+    }
+    public List<BuildingEntry> buildingEntries;
+
     private static GameObject selectedTiles;
     private void OnMouseDown()
-    {
-        if(selectedTiles != gameObject)
+    {        
+        if (selectedTiles != gameObject)
         {
             if (selectedTiles != null)
             {
+                OpenBuildMenu(selectedTiles);
                 listMaterials = selectedTiles.GetComponent<MeshRenderer>().materials.ToList();
                 listMaterials.RemoveAt(1);
                 selectedTiles.GetComponent<MeshRenderer>().materials = listMaterials.ToArray();
@@ -34,8 +41,9 @@ public class ClickLogick : MonoBehaviour
                         continue;
                     listMaterials.RemoveAt(1);
                     selectedTiles.transform.GetChild(i).GetComponent<MeshRenderer>().materials = listMaterials.ToArray();
-                }               
+                }
             }
+            
             meshRenderers = gameObject.GetComponentsInChildren<MeshRenderer>().ToList();
             meshRenderers.Add(gameObject.GetComponent<MeshRenderer>());
             listMaterials = gameObject.GetComponent<MeshRenderer>().materials.ToList();
@@ -52,6 +60,24 @@ public class ClickLogick : MonoBehaviour
                 gameObject.GetComponent<MeshRenderer>().materials = listMaterials.ToArray();
             }
             selectedTiles = gameObject;
+            
+        }
+    }
+
+    private void OpenBuildMenu(GameObject selestedTile)
+    {
+        foreach (var entry in buildingEntries)
+        {
+            entry.button.onClick.RemoveAllListeners();
+            entry.button.onClick.AddListener(() => BuildStructure(entry.building, selestedTile));
+        }
+    }
+
+    private void BuildStructure(GameObject building, GameObject selectedTile)
+    {
+        if (selectedTiles != null)
+        {            
+            Instantiate(building, selectedTile.transform.position, building.transform.rotation);
         }
     }
 }
